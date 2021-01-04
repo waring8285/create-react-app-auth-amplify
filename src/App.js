690a338a1f06223a00ae21cb76d1acce69966007
waring8285 <63865647+waring8285@users.noreply.github.com>
2021-01-03 21:55:41 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,27 +6,40 @@ import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-           This is the future home of the ARETI Enterprise System.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+
+import "../styles/_app.scss";
+import React from "react";
+import { Provider } from "react-redux";
+import { Router } from "react-router-dom";
+import MatxTheme from "./MatxLayout/MatxTheme/MatxTheme";
+import AppContext from "./appContext";
+import history from "history.js";
+
+import routes from "./RootRoutes";
+import { Store } from "./redux/Store";
+import Auth from "./auth/Auth";
+import MatxLayout from "./MatxLayout/MatxLayout";
+import AuthGuard from "./auth/AuthGuard";
+
+const App = () => {
+  return (
+    <AppContext.Provider value={{ routes }}>
+      <Provider store={Store}>
+        <MatxTheme>
+          <Auth>
+            <Router history={history}>
+              <AuthGuard>
+                <MatxLayout />
+              </AuthGuard>
+            </Router>
+          </Auth>
+        </MatxTheme>
+      </Provider>
+    </AppContext.Provider>
+  );
+};
+
+export default App;
+
 
 export default withAuthenticator(App, true);
